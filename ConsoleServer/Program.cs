a3cab6bc0dceefdcf2a61fdca14137c5bd1b1bb1
novakvova <novakvova@gmail>
2022-11-20 16:20:01 +0200

    Begin working socket

---
+++ ConsoleServer/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Net;
+
+Console.WriteLine("Ваш PC "+Dns.GetHostName());
+IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+
+foreach(var ip in ipHostInfo.AddressList)
+{
+    Console.WriteLine("IP "+ip.ToString());
+}
