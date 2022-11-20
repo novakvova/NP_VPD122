9b80ccdfb70758b61c76637cc2554330460670a7
novakvova <novakvova@gmail>
2022-11-20 16:59:57 +0200

    Select ip Server

--- ConsoleServer/Program.cs
+++ ConsoleServer/Program.cs
@@ -1,9 +1,18 @@
 ﻿using System.Net;
 
+Console.InputEncoding= System.Text.Encoding.UTF8;
+Console.OutputEncoding= System.Text.Encoding.UTF8;
 Console.WriteLine("Ваш PC "+Dns.GetHostName());
 IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
-
+Console.WriteLine("Оберіть IP адреся для вашого сервера");
+int i = 0;
 foreach(var ip in ipHostInfo.AddressList)
 {
-    Console.WriteLine("IP "+ip.ToString());
+    Console.WriteLine($"{i} - "+ip.ToString());
+    i++;
 }
+Console.Write("->_");
+IPAddress ip_select = ipHostInfo.AddressList[int.Parse(Console.ReadLine())];
+
+Console.WriteLine("Ви обрали наступний IP " + ip_select);
+
