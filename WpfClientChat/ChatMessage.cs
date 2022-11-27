e871be6fc10bf9dfcd38dd635e0abe10646db2e6
novakvova <novakvova@gmail>
2022-11-27 16:43:23 +0200

    Working Client chat WPF

---
+++ WpfClientChat/ChatMessage.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfClientChat
+{
+    public enum TypeMessage
+    {
+        Login,
+        Logout,
+        Message
+    }
+    public class ChatMessage
+    {
+        public TypeMessage MessageType;
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string Text { get; set; }
+        public byte[] Serialize()
+        {
+            using (var m = new MemoryStream())
+            {
+                using (BinaryWriter writer = new BinaryWriter(m))
+                {
+                    writer.Write((int)MessageType);
+                    writer.Write(UserId);
+                    writer.Write(UserName);
+                    writer.Write(Text);
+                }
+                return m.ToArray();
+            }
+        }
+        public static ChatMessage Desserialize(byte[] data)
+        {
+            ChatMessage message = new ChatMessage();
+            using(MemoryStream m = new MemoryStream(data))
+            {
+                using(BinaryReader reader = new BinaryReader(m))
+                {
+                    message.MessageType = (TypeMessage)reader.ReadInt32();
+                    message.UserId = reader.ReadString();
+                    message.UserName = reader.ReadString();
+                    message.Text = reader.ReadString();
+                }
+            }
+            return message;
+        }
+    }
+}
---
+++ WpfClientChat/MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfClientChat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        TcpClient client = new TcpClient();
+        NetworkStream ns;
+        Thread thread;
+        private ChatMessage _message = new ChatMessage();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string fileName = "config.txt";
+                IPAddress ip;
+                int port;
+                using(FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+                {
+                    using(StreamReader sr = new StreamReader(fs))
+                    {
+                        ip=IPAddress.Parse(sr.ReadLine());
+                        port = int.Parse(sr.ReadLine());
+                    }
+                }
+                _message.UserName=txtUserName.Text;
+                _message.UserId=Guid.NewGuid().ToString();
+                client.Connect(ip,port);
+                lbInfo.Items.Add("Підключення до сервера "+ip.ToString()+":"+port);
+                ns=client.GetStream();
+                thread = new Thread();
+                thread.Start();
+
+                _message.MessageType = TypeMessage.Login;
+                _message.Text="Приєднався до чату";
+                byte[] bytes = _message.Serialize();
+                ns.Write(bytes);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Problem Connection Sever "+ ex.Message);
+            }
+        }
+
+        private void bntSend_Click(object sender, RoutedEventArgs e)
+        {
+            _message.MessageType = TypeMessage.Message;
+            _message.Text = txtText.Text;
+            var buffer = _message.Serialize();
+            ns.Write(buffer, 0, buffer.Length);
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            _message.MessageType = TypeMessage.Message;
+            _message.Text = "Відвалився козак із чату";
+            var buffer = _message.Serialize();
+            ns.Write(buffer, 0, buffer.Length);
+            client.Client.Shutdown(SocketShutdown.Send);
+            thread.Join();
+            ns.Close();
+            client.Close();
+           
+        }
+        private void RecieveData(TcpClient client)
+        {
+            NetworkStream ns = client.GetStream();
+            var receivedBytes = new byte[4128];
+            int byte_count;
+            string data = "";
+            while ((byte_count = ns.Read(receivedBytes, 0, receivedBytes.Length)) > 0)
+            {
+                Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    try
+                    {
+                        ChatMessage message = ChatMessage.Desserialize(receivedBytes);
+                        switch(message.MessageType)
+                        {
+                            case TypeMessage.Message:
+                                {
+                                    lbInfo.Items.Add(message.UserName+" -> "+message.Text);
+                                    break;
+                                }
+                                
+                        }
+                        lbInfo.Items.MoveCurrentToLast();
+                        lbInfo.ScrollIntoView(lbInfo.Items.CurrentItem);
+                    }
+                    catch(Exception ex)
+                    { Console.WriteLine(ex.ToString()); }
+                }));
+            }
+        }
+    }
+}
